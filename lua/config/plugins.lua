1fb32bbf072b8aedf89396f18b4828932ec92d54
allmeta <thomasdasd@gmail.com>
2022-10-17 18:16:35 +0200

    add dashboard

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -75,4 +75,11 @@ return {
     end
   },
 
+  {
+    'goolord/alpha-nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+    config = function ()
+        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    end
+  },
 }
