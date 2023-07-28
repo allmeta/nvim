e30d4565c886c06d908c4a5c93d5f9e7dd9c313a
allmeta <thomal@netcompany.com>
2023-07-28 09:50:53 +0200

    update plugins

--- lua/allmeta/lazy.lua
+++ lua/allmeta/lazy.lua
@@ -13,7 +13,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("allmeta.plugins", {
   defaults = {
-    lazy = true
   },
   install = {
     colorscheme = { "tokyonight-night", "habamax" },
